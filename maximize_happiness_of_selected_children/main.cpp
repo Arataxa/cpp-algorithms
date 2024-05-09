d459e126e8264ec769609c3281100a7a8d41f8d7
Arataxa <131537929+Arataxa@users.noreply.github.com>
2024-05-09 16:56:45 +0500

    Completed task

---
+++ maximize_happiness_of_selected_children/main.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+long long maximumHappinessSum(vector<int> happiness, int k) {
+	long long result = 0;
+
+	partial_sort(happiness.begin(), happiness.begin() + k, happiness.end(), greater<int>());
+
+	while (k--) {
+		result += max(0, happiness[k] - k);
+	}
+
+	return result;
+}
+
+
+int main() {
+	vector<int> happiness = { 4, 1, 2, 1, 7, 8, 9 };
+	int k = 3;
+	cout << "Maximum happiness sum is: " << maximumHappinessSum(happiness, k) << endl;
+}
